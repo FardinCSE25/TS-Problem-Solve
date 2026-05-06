6fa08b3eb5cf15c64f8d196a4a8528b5fa3a0246
Fardin Ahmed <fardin.cse25@gmail.com>
2026-05-06 19:42:51 +0600

    Problem-2 and problem-3 solved

--- solutions.ts
+++ solutions.ts
@@ -1,11 +1,38 @@
-
-const filterEvenNumbers = (arr: number[]) => {
-    const filteredArray = arr.filter((num : number) =>{
-        if(num % 2 === 0){
+// Problem-1 :
+const filterEvenNumbers = (arr: number[]): number[] => {
+    const filteredArray = arr.filter((num: number) => {
+        if (num % 2 === 0) {
             return num;
         }
     })
     return filteredArray;
 }
 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+
+// Problem-2 :
+const reverseString = (text: string) : string => {
+    const singleLettersArray = text.split("")
+    const reversedTextArray = singleLettersArray.reverse().join("");
+    return reversedTextArray;
+}
+
+
+// Problem-3 :
+type StringOrNumber = number | string;
+const checkType = (text: StringOrNumber) => {
+  if(typeof text === "number"){
+    return "Number";
+  }
+  if(typeof text === "string"){
+    return "String";
+  }
+}
+
+
+// Problem-4 :
+
+
+
+
+
